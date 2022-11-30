d006ac0cf55f5d76a7cde27ad7c1212902aceb37
Mark Wanjau <theafrikanson@gmail.com>
2022-11-30 09:08:52 +0300

    passed tests

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function distanceFromHqInBlocks(blocks){
         return 42 - blocks;
     }
 }
-distanceFromHqInBlocks(43);
+// distanceFromHqInBlocks(43);
 
 function distanceFromHqInFeet(blocks){
     if(blocks > 42){
@@ -16,7 +16,7 @@ function distanceFromHqInFeet(blocks){
        return (42 - blocks) * 264;
     }
 }
-distanceFromHqInFeet(distanceFromHqInBlocks(43));
+// distanceFromHqInFeet(distanceFromHqInBlocks(43));
 
 
 function distanceTravelledInFeet(start, destination){
@@ -27,23 +27,25 @@ function distanceTravelledInFeet(start, destination){
     return (destination - start) * 264;
    }
 }
-distanceTravelledInFeet(34, 38);
+// distanceTravelledInFeet(34, 38);
 
 function calculatesFarePrice(start, destination){
-    if((start - destination)* 264 <= 400){
+    let distance = distanceTravelledInFeet(start, destination)
+    
+    if(distance <= 400){
         return 0;
     }
-    else if((start - destination)* 264 > 400 && (start - destination)* 264 <= 2000){
+    else if(distance > 400 && distance <= 2000){
         return 2.56;
     } 
-    else if((start - destination)* 264 > 2000 && (start - destination)* 264 <= 2500){
+    else if(distance > 2000 && distance < 2500){
         return 25;
     }
 
-    else if((start - destination)* 264 > 2500){
+    else {
         return 'cannot travel that far';
     }
 
 }
-  console.log(calculatesFarePrice(40,32));
+//   console.log(distanceFromHqInFeet(50,58));
 
